ae56551b23c752076b3409914506b48b05112843
Mike Faber <mi3406@fabermmac-lt.home>
2020-09-25 16:02:26 -0500

    Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿namespace Gone.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        [Route("{*url}")]
+        public IActionResult Index()
+        {
+            return StatusCode(410);
+        }
+    }
+}
